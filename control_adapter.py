2a2167670a238164dc0d18eb18dd1eed1e49cd3f
Alexande Becker <a.becker@cern.ch>
2021-12-02 12:20:01 +0000

    simplified the zmq_controller to only concern itself with sending messages

---
+++ control_adapter.py
@@ -0,0 +1,145 @@
+import zmq
+import yaml
+from time import sleep
+from config_utilities import patch_configuration
+from config_utilities import diff_dict
+
+
+class ControlAdapter:
+    """
+    Base class that encapsulates the configuration and communication to
+    one of the daq-system components
+    """
+    def __init__(self, hostname, port, config):
+        """
+        Initialize the data structure on the control computer (the one coordinating everything)
+        and connect to the system component. Do not load any configuraion yet
+        """
+        self.hostname = hostname
+        self.port = port
+        self.context = zmq.Context()
+        self.socket = self.context.socket(zmq.REQ)
+        self.socket.connect(f"tcp://{self.hostname}:{self.port}")
+        self.confifguration = config
+
+    def reset(self):
+        """
+        reset the connection with the system component, may not reset the state of the component
+        """
+        self.socket.close()
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect(f"tcp://{self.hostname}:{self.port}")
+
+    def configure(self, config):
+        """
+        send the configuration to the corresponding system component and wait for the
+        configuration to be completed
+        """
+        config_diff = diff_dict(self.confifguration, config)
+        self.socket.send_string("configure")
+        rep = self.socket.recv_string()
+        if "ready" not in rep.lower():
+            raise ValueError(
+                "The configuration cannot be "
+                f" written to {self.hostname}. The target"
+                f"responded with {rep}")
+        self.socket.send_string(yaml.dump(config_diff))
+        rep = self.socket.recv_string()
+        print(rep)
+
+
+class TargetAdapter(ControlAdapter):
+    """
+    The adapter that is used to control the Targets (so ROCs and
+    Hexboards)
+    """
+    def read_config(self, parameter: dict):
+        """
+        Read the values set on the ROC directly from it
+
+        Arguments:
+            paramter, dict: The parameter that should be read from the ROC
+
+        Returns:
+            the dict containing the requested parameter(s) passed in the parameter
+            argument set to the value read from the ROC
+        """
+        self.socket.send_string("read")
+        rep = self.socket.recv_string()
+        if yamlNode:
+            self.socket.send_string(yaml.dump(parameter))
+        else:
+            self.socket.send_string("")
+        return yaml.safe_load(self.socket.recv_string())
+
+    def read_pwr(self):
+        # only valid for hexaboard/trophy systems
+        self.socket.send_string("read_pwr")
+        rep = self.socket.recv_string()
+        pwr = yaml.safe_load(rep)
+        return(pwr)
+
+    def resettdc(self):
+        self.socket.send_string("resettdc")
+        rep = self.socket.recv_string()
+        return(yaml.safe_load(rep))
+
+    def measadc(self, yamlNode):
+        # only valid for hexaboard/trophy systems
+        self.socket.send_string("measadc")
+        rep = self.socket.recv_string()
+        if rep.lower().find("ready") < 0:
+            print(rep)
+            return
+        if yamlNode:
+            config = yamlNode
+        else:
+            config = self.yamlConfig
+        self.socket.send_string(yaml.dump(config))
+        rep = self.socket.recv_string()
+        adc = yaml.safe_load(rep)
+        return(adc)
+
+
+class DAQAdapter(ControlAdapter):
+    """
+    Encapsulate the Access to the daq-client and server of the daq system.
+    """
+
+    def start(self):
+        """
+        Start the aquisition of the data on the server and client
+        """
+        rep = ""
+        while "running" not in rep.lower():
+            self.socket.send_string("start")
+            rep = self.socket.recv_string()
+            print(rep)
+
+    def is_done(self):
+        """
+        check if the current aquisition is ongoing or not
+        """
+        self.socket.send_string("run_done")
+        rep = self.socket.recv_string()
+        if "notdone" in rep:
+            return False
+        else:
+            return True
+
+    def stop(self):
+        """
+        stop the currently running measurement
+        """
+        self.socket.send_string("stop")
+        rep = self.socket.recv_string()
+        print(rep)
+
+    def delay_scan(self):
+        # only for daq server to run a delay scan
+        rep = ""
+        while "delay_scan_done" not in rep:
+            self.socket.send_string("delayscan")
+            rep = self.socket.recv_string()
+            print(rep)
