683b140b09442728af1c101a5fe0e6159b7c342f
Alexande Becker <a.becker@cern.ch>
2021-12-08 09:21:52 +0000

    tested the identifier scheme and passing the input of a task to it's output

--- luigi_tests.py
+++ luigi_tests.py
@@ -2,25 +2,31 @@ import luigi
 from luigi.parameter import ParameterVisibility
 from time import sleep
 from pathlib import Path
+import operator
+from functools import reduce
 
 class scan(luigi.Task):
     parameters = luigi.ListParameter(significant=True)
     base_path = luigi.Parameter()
-    level = luigi.IntParameter(significant=True, default=0)
+    identifier = luigi.IntParameter(significant=True, default=0)
 
     def requires(self):
         tasks = []
         print(self.parameters)
         if len(self.parameters) > 1:
+            task_id_offset = reduce(operator.mul,
+                                    [len(param) for param in
+                                     self.parameters[1:]])
             print("add a level of recursion")
-            for path_suffix in self.parameters[0]:
+            for i, path_suffix in enumerate(self.parameters[0]):
                 tasks.append(scan(self.parameters[1:],
                                   self.base_path+path_suffix,
-                                  self.level+1))
+                                  self.identifier + 1 + (task_id_offset * i)))
         else:
             print("lowest level, add the actual tasks")
-            for path in self.parameters[0]:
-                tasks.append(task_with_file_out(self.base_path+path))
+            for i, path in enumerate(self.parameters[0]):
+                tasks.append(Format(self.base_path+path,
+                                    self.identifier + i))
         return tasks
 
     def run(self):
@@ -31,21 +37,51 @@ class scan(luigi.Task):
                 scan_in.close()
 
     def output(self):
-        return luigi.LocalTarget(f'output_level{self.level}')
+        return luigi.LocalTarget(f'output_level{self.identifier}')
 
-
-class task_with_file_out(luigi.Task):
+class Format(luigi.Task):
     path = luigi.Parameter(significant=True)
+    identifier = luigi.Parameter(significant=True)
 
-    def run(self):
-        p = Path(self.path)
-        sleep(3)
-        with self.output().open('w') as f:
-            f.write("test")
+    def requires(self):
+        return pseudo_measurement(self.path, self.identifier)
 
     def output(self):
-        return luigi.LocalTarget(self.path)
+        return luigi.LocalTarget(self.path + '_' + str(self.identifier) + '.hdf5')
+
+    def run(self):
+        with self.output().open('w') as format_out:
+            for measurement_out in self.input():
+                with measurement_out.open('r') as inp:
+                    format_out.write(inp.read())
+
+class pseudo_measurement(luigi.Task):
+    path = luigi.Parameter(significant=True)
+    identifier = luigi.Parameter(significant=True)
+
+    def requires(self):
+        return pseudo_configuration(self.path, self.identifier)
+
+    def run(self):
+        with self.output()[0].open('w') as f:
+            f.write("example data")
+
+    def output(self):
+        return (luigi.LocalTarget(self.path+'_'+str(self.identifier)+'-data'), self.input())
+
+
+class pseudo_configuration(luigi.Task):
+    path = luigi.Parameter(significant=True)
+    identifier = luigi.Parameter(significant=True)
+
+    def run(self):
+        with self.output().open('w') as config_file:
+            config_file.write('example config')
+
+    def output(self):
+        return luigi.LocalTarget(self.path+'_'+str(self.identifier)+'-config')
+
 
 if __name__ == "__main__":
-    run_result = luigi.build([scan([[f'a={i}_' for i in range(3)], [f'b={i}_' for i in range(3)], [f'c={i}' for i in range(3)]], './')], local_scheduler=True)
+    run_result = luigi.build([scan([[f'a={i}_' for i in range(3)], [f'b={i}_' for i in range(3)], [f'c={i}' for i in range(3)]], './', 1)], local_scheduler=True)
     print(run_result)
