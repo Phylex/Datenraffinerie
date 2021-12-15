843437865b51bfc5b3d779772e85cd004851b936
Alexande Becker <a.becker@cern.ch>
2021-12-15 12:32:29 +0000

    adapted the paths for the data out

--- test_datenraffinerie.py
+++ test_datenraffinerie.py
@@ -4,4 +4,4 @@ from valve_yard import ValveYard
 if __name__ == '__main__':
     RUN_RESULT = luigi.build([ValveYard(
         './test_configurations/main_config.yaml',
-        'timewalk_scan', 'test_out')])
+        'timewalk_scan', './test_out')])
