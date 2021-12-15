70fd495a7a14902073c4c5b7b5ff54600a17421a
Alexande Becker <a.becker@cern.ch>
2021-12-15 08:37:38 +0000

    first test of the datenraffinerie DAQ

---
+++ test_datenraffinerie.py
@@ -0,0 +1,7 @@
+import luigi
+from valve_yard import ValveYard
+
+if __name__ == '__main__':
+    RUN_RESULT = luigi.build([ValveYard(
+        './test_configurations/main_config.yaml',
+        'timewalk_scan', 'test_out')])
