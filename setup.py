30448e1fa4c577f6330772dcfb5b7f004aa5f7e2
Alexande Becker <a.becker@cern.ch>
2022-01-10 14:48:17 +0000

    added packaging metainformation files

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    entry_points={
+        'console_scripts': [
+            'datenraffinerie = datenraffinerie.datenraffinerie:cli',
+        ]
+    },
+    install_requires=[
+        'Click',
+        'luigi',
+        'pandas',
+        'matplotlib',
+        'numpy',
+        'scipy',
+        'uproot',
+        'pyyaml'
+    ]
+)
