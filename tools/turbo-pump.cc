69d8c1fef874beed65d0e8f8847a7a65ab3ec5f9
Alexande Becker <a.becker@cern.ch>
2022-05-05 14:31:19 +0000

    moved turbo pump over to the new pytables hdf routines

--- tools/turbo-pump.cc
+++ tools/turbo-pump.cc
@@ -30,10 +30,14 @@ int main(int argc, char **argv) {
 		std::cout << "More than one input file needs to be specified" << std::endl;
 		exit(EXIT_FAILURE);
 	}
-	hid_t output = create_merge_output_file(output_path, group_name, input_file_paths[0]);
-	for (size_t i = 1; i < input_file_paths.size(); i++) {
+	hid_t input_table_type = get_pytable_type(input_file_paths[0], group_name, "measurements");
+	hid_t output_file = create_pytables_file(output_path);
+	hid_t output_group = create_pytables_group(output_file, group_name, "");
+	hid_t output_talbe = create_pytables_table(output_group, "measurements", input_table_type, block_size);
+	for (size_t i = 0; i < input_file_paths.size(); i++) {
 		try {
-			append_file(output, group_name, input_file_paths[i]);
+			hid_t in_file = H5Fopen(input_file_paths[i].c_str(), H5P_DEFAULT, H5P_DEFAULT);
+			merge_tables(output_file, in_file, group_name, "measurements", block_size);
 		} catch (std::runtime_error e) {
 			std::cout << "Runtime error appending file: " << input_file_paths[i] << std::endl << e.what() << std::endl;
 			exit(EXIT_FAILURE);
