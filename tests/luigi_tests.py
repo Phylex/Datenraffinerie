9c2d128b7308e9727d81968e7ad8890a8c31d006
Alexande Becker <a.becker@cern.ch>
2022-06-27 10:23:18 +0200

    tested multiprocessing inside a luigi task

--- tests/luigi_tests.py
+++ tests/luigi_tests.py
@@ -1,4 +1,4 @@
-from multiprocessing import Lock
+from multiprocessing import Lock, Process
 import luigi
 from pid import PidFile
 from luigi.parameter import ParameterVisibility
@@ -88,8 +88,40 @@ class pseudo_configuration(luigi.Task):
         return luigi.LocalTarget(self.path+'_'+str(self.identifier)+'-config')
 
 
+class test_mp_task(luigi.Task):
+    id = luigi.IntParameter(significant=False, default=42)
+
+    def requires(self):
+        return None
+
+    @staticmethod
+    def parallel_part(target):
+        seconds = list(range(target))
+        seconds.reverse()
+        for i in seconds:
+            print(i)
+            sleep(1)
+        return
+
+    def run(self):
+        workers = []
+        for i in range(self.id):
+            p = Process(target=self.parallel_part, args=(i+1,))
+            workers.append(p)
+            p.start()
+        for w in workers:
+            w.join()
+        print("run all workers")
+        with self.output().open('w') as outf:
+            outf.write('done')
+
+    def output(self):
+        return luigi.LocalTarget('./out.test')
+
+
 if __name__ == "__main__":
-    run_result = luigi.build([scan([[f'a={i}_' for i in range(3)], [f'b={i}_' for i in range(3)], [f'c={i}' for i in range(3)]], './', 1)],
-                             local_scheduler=True,
-                             workers=5)
+    run_result = luigi.build([test_mp_task(5)], local_scheduler=True, workers=1)
+    # run_result = luigi.build([scan([[f'a={i}_' for i in range(3)], [f'b={i}_' for i in range(3)], [f'c={i}' for i in range(3)]], './', 1)],
+    #                          local_scheduler=True,
+    #                          workers=5)
     print(run_result)
