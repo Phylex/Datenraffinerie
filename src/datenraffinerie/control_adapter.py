91f49414d0f74130ed96f958870da5d6c0afdd76
Alexande Becker <a.becker@cern.ch>
2022-08-03 17:49:36 +0200

    debugged control adapter and tested it

--- src/datenraffinerie/control_adapter.py
+++ src/datenraffinerie/control_adapter.py
@@ -57,14 +57,16 @@ class DAQAdapter():
                  port: int):
         # set up the logger for this object
         self.logger = logging.getLogger(
-                __name__+'.TargetAdapter')
+                __name__+f'.{variant}')
 
         # initialize the connection to the target server
         self.hostname = hostname
         self.port = port
         self.context = zmq.Context()
         self.socket = self.context.socket(zmq.REQ)
-        self.socket.connect(f"tcp://{self.hostname}:{self.port}")
+        socket_url = f'tcp://{self.hostname}:{self.port}'
+        self.logger.debug(f'connecting to {socket_url}')
+        self.socket.connect(socket_url)
         self.variant = variant
         self.running = False
 
@@ -92,17 +94,19 @@ class DAQAdapter():
     def initialize(self, initial_config: dict):
         initial_config, _, _ = _filter_out_network_config(initial_config)
         self.logger.debug("initializing")
-        self.configuration = initial_config
-        self.socket.send_string("initalize", zmq.SNDMORE)
+        update_dict(self.configuration,
+                    self._clean_configuration(initial_config), in_place=True)
+        self.socket.send_string("initialize", zmq.SNDMORE)
         config_string = yaml.dump(self.configuration)
         self.logger.debug("sending config:\n" + config_string)
         self.socket.send_string(config_string)
-        rep = self.socket.recv_string()
+        rep = self.socket.recv()
+        rep = rep.decode('utf-8')
         if rep != "initialized":
-            raise ValueError("Server not successfully initialized")
             self.logger.critical(
                     "Server not successfully initialized."
                     f" Received {rep}")
+            raise ValueError("Server not successfully initialized")
         else:
             self.logger.debug(f"Received reply: {rep}")
 
@@ -119,7 +123,7 @@ class DAQAdapter():
             write_config = config
 
         # if the config is empty we don't have to write anything
-        if write_config == {}:
+        if write_config == {} or write_config is None:
             self.logger.debug("No new configuration to transmit")
             return
         config_string = yaml.dump(write_config)
@@ -150,18 +154,32 @@ class DAQAdapter():
         """
         self.logger.info("Starting data-taking")
         status = "none"
+        self.logger.debug(f'Checking status of {self.variant}')
         self.socket.send_string("status")
         status = self.socket.recv_string()
-        if status == "configured":
-            self.socket.send_string("start")
-        else:
+        self.logger.debug(f'Current state: {status}')
+        if status != "configured":
             raise ValueError("Server not ready to start data taking")
-        status = self.socket.recv_string()
-        self.logger.debug(f"Status received from Server: {status}")
-        if status != "running":
-            raise ValueError(f"Server not running, status: {status}")
-        self.info("Data is being aquired")
-        return
+        retries = 50
+        post_link_alignment_state = 'configured'
+        while retries > 0:
+            self.logger.debug(
+                    f'Attempting to start backend, {retries} trys remaining')
+            self.socket.send_string("start")
+            status = self.socket.recv_string()
+            if status == 'running':
+                post_link_alignment_state = status
+                break
+            retries -= 1
+        # if there where no trys left
+        if post_link_alignment_state != 'running':
+            self.logger.critical(
+                    'Unable to start server, probably bad link alignment')
+            raise ValueError(
+                    'Unable to Start server, probably bad link alignment')
+        self.logger.info(
+            f"successfully started backend, current state: {status}")
+        return status
 
     def stop(self):
         self.logger.info("Stopping data-taking")
@@ -177,16 +195,22 @@ class DAQAdapter():
         check if the current aquisition is ongoing or not
         """
         self.logger.info('Starting data taking run')
-        self.start()
-        self.socket.send_string("status")
-        status = self.socket.recv_string()
-        self.logger.debug(f"Checking if run is done, received: {status}")
+        # attempt to start server. Server only switches into the
+        # running state if the links are aligned.
+        # so we query until we run out of tries or the server switches to
+        # running
+        self.logger.info('links aligned, taking data')
+        status = self.start()
+        self.logger.info('waiting for run to finish')
         while status == "running":
             self.socket.send_string("status")
             status = self.socket.recv_string()
         if status == "configured":
+            self.logger.info('Data taking run completed')
             return
         else:
+            self.logger.critical(
+                'Invalid status from the DAQ server: {status}')
             raise ValueError('Invalid status from the DAQ server: {status}')
 
 
@@ -219,8 +243,8 @@ class DAQSystem:
         self.tear_down_data_taking_context()
 
     def initalize(self, initial_config: dict):
-        self.server.initialize(initial_config)
         self.client.initialize(initial_config)
+        self.server.initialize(initial_config)
 
     def configure(self, daq_config: dict = None):
         """
@@ -230,47 +254,35 @@ class DAQSystem:
         self.server.configure(daq_config, cache=True)
 
     def setup_data_taking_context(self):
-        """
-        setup the folders and the zmq-client configuration
-
-        Function is called at initialisation and should not be called
-        by user code
-
-        Prepare a folder to save the raw data in and set up the client
-        configuration so that the zmq-client writes into that folder
-        It is expected that the folder is empty before every measurement
-        as the filename of the zmq-client is not easily predictable
-        """
-        # get the location for the placement of the files by the
-        # zmq-client, if one is already configured then use it
-        # otherwise generate a new one
         client_config = {}
+        self.logger.debug('Setting up data taking context')
         self.procedure_uuid = uuid.uuid1().hex
         self.daq_data_folder = Path('/tmp') / self.procedure_uuid
         client_config['outputDirectory'] = str(self.daq_data_folder)
         client_config['run_type'] = "Datenraffinerie"
-        self.client.update_config(
-            {self.variant_key_map[self.variant]: client_config})
         if not os.path.isdir(self.daq_data_folder):
             os.mkdir(self.daq_data_folder)
-        self.client.configure(cache=False)
+        self.client.configure(
+                {self.client.variant_key_map[self.client.variant]:
+                 client_config})
+        self.logger.debug('Updated client configuration to\n' +
+                          yaml.safe_dump(self.client.configuration))
+        active_menu = self.server.configuration['daq']['active_menu']
+        config_transition_server_config = {}
+        config_transition_server_config['daq'] = {}
+        config_transition_server_config['daq']['active_menu'] = active_menu
+        config_transition_server_config['daq']['menus'] = {}
+        config_transition_server_config['daq']['menus'][active_menu] = \
+            self.server.configuration['daq']['menus'][active_menu]
+        self.server.configure(config_transition_server_config)
 
     def take_data(self, output_data_path):
-        """
-        function that encapsulates the data taking currently done via the
-        zmq-client program. The zmq-client currently has a particular way of
-        naming the files it creates that is incompatible with the way luigi
-        expects the files to be named to be able to evaluate if a task has
-        completed or not.
-
-        The strategy here is to configure the zmq-client to put it's output
-        into the /tmp folder of the machine running the client and the Daten-
-        raffinerie and then to copy that file from the location in tmp to
-        the location given by the 'output_data_path' argument of the function
-        after the daq for the run has concluded
-        """
         self.client.start()
-        self.server.take_data()
+        try:
+            self.server.take_data()
+        except ValueError as err:
+            self.client.stop()
+            raise ValueError('Data taking failed') from err
         data_files = os.listdir(self.daq_data_folder)
         if len(data_files) > 1:
             raise ValueError(
@@ -278,14 +290,9 @@ class DAQSystem:
                     f" {self.daq_data_folder.resolve()} folder")
         data_file = data_files[0]
         shutil.move(self.daq_data_folder / data_file, output_data_path)
-        self.server.stop()
         self.client.stop()
 
     def tear_down_data_taking_context(self):
-        """
-        The complement to the 'start_run' function stops the run and cleans up
-        after the run has completed
-        """
         try:
             if os.path.exists(self.daq_data_folder):
                 for file in self.daq_data_folder.iterdir():
