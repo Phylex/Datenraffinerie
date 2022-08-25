860da60d2975c505683c34da1928242031197ef1
Alexande Becker <a.becker@cern.ch>
2022-08-25 12:36:44 +0200

    added a main function that runs the daq in pipelined mode

--- src/datenraffinerie/acquire_data.py
+++ src/datenraffinerie/acquire_data.py
@@ -156,3 +156,114 @@ def pipelined_acquire_data(configurations: queue.Queue,
         )
         i += 1
     data_acquisition_done.set()
+
+
+@click.command()
+@click.argument('output_directory', type=click.Path(dir_okay=True),
+                metavar='[OUTPUT DIR]')
+@click.option('--log', default='daq.log', type=str,
+              help='Enable logging by specifying the output logfile')
+@click.option('--loglevel', default='INFO',
+              type=click.Choice(['DEBUG', 'INFO',
+                                 'WARNING', 'ERROR', 'CRITICAL'],
+                                case_sensitive=False),
+              help='specify the logging verbosity')
+@click.option('--keep/--no-keep', default=False,
+              help='Keep the already aquired data, defaults to False')
+def pipelined_main(output_directory, log, loglevel, keep):
+    if log is not None:
+        logging.basicConfig(filename=log, level=_log_level_dict[loglevel],
+                            format='[%(asctime)s] %(levelname)s:'
+                                   '%(name)-50s %(message)s')
+    logger = logging.getLogger('main')
+    # get the expected files from the directory
+    output_directory = Path(output_directory)
+    network_config = output_directory / 'network_config.yaml'
+    if not network_config.exists():
+        print(f"No network_config.yaml found in {output_directory}"
+              "exiting ...")
+        exit(1)
+    default_config = output_directory / 'default_config.yaml'
+    if not default_config.exists():
+        print(f"No default_config.yaml found in {output_directory}"
+              "exiting ...")
+        exit(1)
+    init_config = output_directory / 'initial_state_config.yaml'
+    if not init_config.exists():
+        print(f"No initial_state_config.yaml found in {output_directory}"
+              "exiting ...")
+        exit(1)
+    run_config_files = list(glob.glob(
+        str(output_directory.absolute()) + '/' + 'run_*_config.yaml'))
+    sorted(run_config_files,
+           key=lambda x: int(os.path.basename(x).split('_')[1]))
+    run_config_files = [output_directory / Path(rcf)
+                        for rcf in run_config_files]
+    run_indices = list(map(lambda x: os.path.basename(x).split('_')[1],
+                           run_config_files))
+    full_run_config_files = [rcf.parent / (rcf.stem + '_full.yaml')
+                             for rcf in run_config_files]
+
+    # read in the configurations
+    logger.info('Reading in configurations')
+    with open(network_config, 'r') as nwcf:
+        network_config = yaml.safe_load(nwcf.read())
+    with open(default_config, 'r') as dcf:
+        default_config = yaml.safe_load(dcf.read())
+    with open(init_config, 'r') as icf:
+        init_config = yaml.safe_load(icf.read())
+    # set up the different events that we are looking for
+    all_run_configs_generated = threading.Event()
+    daq_system_initialized = threading.Event()
+    daq_done = threading.Event()
+
+    # set up the queues to pass the data around
+    run_configuration_queue = queue.Queue()
+    daq_progress_queue = queue.Queue()
+    acquired_data_queue = queue.Queue()
+    daq_thread = threading.Thread(
+            target=pipelined_acquire_data,
+            args=(run_configuration_queue,
+                  daq_progress_queue,
+                  acquired_data_queue,
+                  all_run_configs_generated,
+                  daq_system_initialized,
+                  network_config,
+                  init_config,
+                  output_directory,
+                  len(run_indices),
+                  keep,
+                  )
+            )
+    daq_thread.start()
+    with Progress() as progress:
+        read_configurations = progress.add_task(
+                "[cyan] Read run configurations from disk",
+                total=len(run_indices))
+        daq_progbar = progress.add_task(
+                "[turquoise]Acquiring Data from the Test System",
+                total=len(run_indices),
+                start=False
+                )
+        for rcfp, frcfp in zip(run_config_files, full_run_config_files):
+            if daq_system_initialized.is_set():
+                progress.start_task(daq_progbar)
+            with open(rcfp, 'r') as rcf:
+                run_configuration_queue.put(
+                        (yaml.safe_load(rcf.read()), frcfp)
+                )
+            progress.update(read_configurations, advance=1)
+        all_run_configs_generated.set()
+        if not daq_system_initialized.is_set():
+            daq_system_initialized.wait()
+            progress.start_task(daq_progbar)
+        while not daq_done.is_set():
+            try:
+                daq_tasks_completed = daq_progress_queue.get(block=False)
+                progress.update(daq_progbar, advance=daq_tasks_completed)
+            except queue.Empty:
+                pass
+            try:
+                _ = acquired_data_queue.get(block=False)
+            except queue.Empty:
+                pass
