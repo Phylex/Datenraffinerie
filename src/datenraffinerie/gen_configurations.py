32336d2cb04b538451ad6f61acc86baf3ed9168e
Alexande Becker <a.becker@cern.ch>
2022-08-30 21:48:20 +0200

    added multiprocessing for quicker full config generation

--- src/datenraffinerie/gen_configurations.py
+++ src/datenraffinerie/gen_configurations.py
@@ -20,6 +20,14 @@ from itertools import tee
 from time import sleep
 import logging
 from typing import Union
+from operator import and_
+from functools import reduce
+
+_log_level_dict = {'DEBUG': logging.DEBUG,
+                   'INFO': logging.INFO,
+                   'WARNING': logging.WARNING,
+                   'ERROR': logging.ERROR,
+                   'CRITICAL': logging.CRITICAL}
 
 
 @click.command()
@@ -30,11 +38,23 @@ from typing import Union
                 metavar='[Procedure to be run by the datenraffinerie]')
 @click.argument('output_dir', type=click.Path(dir_okay=True),
                 metavar='[Location to write the configuration files to]')
-def generate_configuratons(config, netcfg, procedure, output_dir):
+@click.option('--full_conf_generators', '-f', type=int, default=1,
+              help='set how many full-cfg generators to run, default 1')
+@click.option('--log/--no-log', type=bool, default=True,
+              help='Enable/Disable logging')
+@click.option('--loglevel', default='INFO',
+              type=click.Choice(['DEBUG', 'INFO',
+                                 'WARNING', 'ERROR', 'CRITICAL'],
+                                case_sensitive=False),
+              help='specify the logging verbosity')
+def generate_configuratons(config, netcfg, procedure,
+                           output_dir, full_conf_generators, log,
+                           loglevel):
     # generate the conifgurations
-    logging.basicConfig(filename='gen_config.log', level='INFO',
-                        format='[%(asctime)s] %(levelname)s:'
-                               '%(name)-50s %(message)s')
+    if log:
+        logging.basicConfig(filename='gen_config.log', level=loglevel,
+                            format='[%(asctime)s] %(levelname)s:'
+                                   '%(name)-50s %(message)s')
     config = click.format_filename(config)
     try:
         procedure, (system_default_config, system_init_config, run_configs,
@@ -77,35 +97,48 @@ def generate_configuratons(config, netcfg, procedure, output_dir):
     # generate the configurations for the runs
     num_digits = math.ceil(math.log(run_count, 10))
     full_config = dctu.update_dict(system_default_config, system_init_config)
+
+    # generate the starting config for multiple full_conf generators
+    run_config_queues = [mp.Queue() for _ in range(full_conf_generators)]
+    generator_config_states = [{} for _ in range(full_conf_generators)]
+    full_config_generators_done = [
+            mp.Event() for _ in range(full_conf_generators)]
+
     run_param_queue = queue.Queue()
     param_gen_progress_queue = queue.Queue()
-    run_config_generation_done = mp.Event()
-    run_config_queue = mp.Queue()
+    config_queue_filled = mp.Event()
+    run_config_gen_done = threading.Event()
     full_configs_generated = mp.Queue()
-    full_config_generation_done = mp.Event()
+    stop = mp.Event()
+    config_iter_lock = threading.Lock()
+
     config_gen_thread = threading.Thread(
             target=pipelined_generate_run_params,
             args=(output_dir,
-                  run_configs_1,
+                  run_configs_2,
+                  config_iter_lock,
                   run_param_queue,
                   param_gen_progress_queue,
-                  run_config_generation_done,
+                  run_config_gen_done,
                   num_digits
                   )
             )
-    full_config_gen_proc = mp.Process(
+    full_config_gen_procs = [mp.Process(
             target=generate_full_configs,
             args=(output_dir,
-                  run_config_queue,
-                  run_config_generation_done,
+                  rcq,
+                  config_queue_filled,
                   full_config,
                   num_digits,
                   full_configs_generated,
-                  full_config_generation_done
+                  fcgd,
+                  stop
                   )
-            )
+            ) for rcq, fcgd in zip(run_config_queues,
+                                   full_config_generators_done)]
     config_gen_thread.start()
-    full_config_gen_proc.start()
+    for fcgp in full_config_gen_procs:
+        fcgp.start()
     with Progress(
             SpinnerColumn(),
             TextColumn("[progress.description]{task.description}"),
@@ -119,70 +152,94 @@ def generate_configuratons(config, netcfg, procedure, output_dir):
         full_progress_bar = progress.add_task(
                 'Generating full config for fracker',
                 total=run_count)
-        while not run_config_generation_done.is_set() \
+        i = 0
+        run_config = {}
+        while not config_queue_filled.is_set() \
                 or not param_gen_progress_queue.empty() \
                 or not run_param_queue.empty() \
-                or not full_config_generation_done.is_set()\
-                or not full_configs_generated.empty():
+                or not run_config_gen_done.is_set()\
+                or not reduce(and_, map(lambda x: x.is_set(),
+                                        full_config_generators_done)):
             try:
-                run_config_queue.put(next(run_configs_2))
+                for j, (rcq, gstate) in enumerate(
+                        zip(run_config_queues, generator_config_states)):
+                    with config_iter_lock:
+                        dctu.update_dict(run_config,
+                                         next(run_configs_1), in_place=True)
+                    new_state = dctu.update_dict(gstate, run_config)
+                    rcq.put((i, dctu.diff_dict(gstate, new_state)))
+                    i += 1
+                    generator_config_states[j] = new_state
             except StopIteration:
-                pass
+                config_queue_filled.set()
             try:
                 _ = run_param_queue.get(block=False)
             except queue.Empty:
                 pass
-            try:
-                _ = param_gen_progress_queue.get(block=False)
-                progress.update(run_progress_bar, advance=1)
-            except queue.Empty:
-                pass
-            try:
-                _ = full_configs_generated.get(block=False)
-                progress.update(full_progress_bar, advance=1)
-            except queue.Empty:
-                pass
-            sleep(.05)
+            while True:
+                try:
+                    _ = param_gen_progress_queue.get(block=False)
+                    progress.update(run_progress_bar, advance=1)
+                except queue.Empty:
+                    break
+            while True:
+                try:
+                    _ = full_configs_generated.get(block=False)
+                    progress.update(full_progress_bar, advance=1)
+                except queue.Empty:
+                    break
+            logging.debug(
+                    f'{reduce(and_, map(lambda x: x.is_set(), full_config_generators_done))}'
+                    ' = state of full config generators')
+        progress.refresh()
     config_gen_thread.join()
-    full_config_gen_proc.join()
+    for fgp in full_config_gen_procs:
+        fgp.join()
 
 
 def generate_full_configs(output_dir: Path,
                           run_configs: mp.Queue,
-                          run_config_generation_done: mp.Event,
+                          run_queue_fill_done: mp.Event,
                           full_init_config: dict,
                           num_digits: int,
                           generated_configs_full_queue: mp.Queue,
-                          config_generation_full_done: mp.Event):
+                          config_generation_full_done: mp.Event,
+                          stop: mp.Event):
     logger = logging.getLogger('run-config-generator')
     full_config = full_init_config
-    i = 0
-    while not run_configs.empty() or not run_config_generation_done.is_set():
-        config = run_configs.get()
+    while (not run_configs.empty() or not run_queue_fill_done.is_set())\
+            and not stop.is_set():
+        i, config = run_configs.get()
         dctu.update_dict(full_config, config, in_place=True)
         full_run_conf_file_name = 'run_{0:0>{width}}_config_full.yaml'.format(
                 i, width=num_digits)
         with open(output_dir / full_run_conf_file_name, 'w+') as frcf:
             frcf.write(yaml.safe_dump(full_config))
         generated_configs_full_queue.put(output_dir / full_run_conf_file_name)
-        i += 1
         logger.debug(f'{run_configs.empty()} = run_empty')
         logger.debug(
-                f'{run_config_generation_done.is_set()} '
+                f'{run_queue_fill_done.is_set()} '
                 '= config_generation_done')
     config_generation_full_done.set()
 
 
 def pipelined_generate_run_params(
         output_dir: Path,
-        run_configs: list,
+        run_configs: iter,
+        run_conf_lock: threading.Lock,
         config_queue: queue.Queue,
         config_gen_progress_queue: queue.Queue,
-        config_generation_done: Union[mp.Event, threading.Event],
+        run_config_gen_done: Union[mp.Event, threading.Event],
         num_digits: int):
-    for i, run_config in enumerate(run_configs):
+    i = 0
+    while True:
+        with run_conf_lock:
+            try:
+                run_config = next(run_configs)
+            except StopIteration:
+                break
         run_conf_file_name = \
-                'run_{0:0>{width}}_config.yaml'.format(i, width=num_digits)
+            'run_{0:0>{width}}_config.yaml'.format(i, width=num_digits)
         full_run_conf_file_name = 'run_{0:0>{width}}_config_full.yaml'.format(
                 i, width=num_digits)
         raw_file_name = \
@@ -206,4 +263,5 @@ def pipelined_generate_run_params(
                  full_run_conf_path)
         )
         config_gen_progress_queue.put(i)
-    config_generation_done.set()
+        i += 1
+    run_config_gen_done.set()
