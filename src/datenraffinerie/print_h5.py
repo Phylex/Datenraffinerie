65ec69f99ed9411c6c13ae31bf5b117a5e7acb76
Alexande Becker <a.becker@cern.ch>
2022-09-12 11:34:47 +0200

    added capability to look at rows

--- src/datenraffinerie/print_h5.py
+++ src/datenraffinerie/print_h5.py
@@ -7,8 +7,12 @@ from rich import print
 @click.command()
 @click.argument('hdf-file', type=click.Path(exists=True),
                 metavar='[HDF-DATA]')
-def show_hdf(hdf_file):
+@click.option('-+rows', '-r', type=int, default=None)
+def show_hdf(hdf_file, rows):
     tb = tables.open_file(hdf_file)
     data = tb.root.data.measurements
     df = pd.DataFrame.from_records(data.read())
-    print(df)
+    if rows is not None:
+        print(df.head(rows))
+    else: 
+        print(df)
