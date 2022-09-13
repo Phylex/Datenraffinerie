ac53b53c3cccb9199aec150283d4c8f699613b66
Alexande Becker <a.becker@cern.ch>
2022-09-13 16:11:56 +0200

    added the full readback option and the tdc start option to the acquire data script

--- src/datenraffinerie/acquire_data.py
+++ src/datenraffinerie/acquire_data.py
@@ -21,102 +21,6 @@ _log_level_dict = {'DEBUG': logging.DEBUG,
                    'CRITICAL': logging.CRITICAL}
 
 
-@click.command()
-@click.argument('output_directory', type=click.Path(dir_okay=True),
-                metavar='[directory containing the run configurations]')
-@click.option('--log', default=None, type=str,
-              help='Enable logging by specifying the output logfile')
-@click.option('--loglevel', default='INFO',
-              type=click.Choice(['DEBUG', 'INFO',
-                                 'WARNING', 'ERROR', 'CRITICAL'],
-                                case_sensitive=False),
-              help='specify the logging verbosity')
-@click.option('--keep/--no-keep', default=False,
-              help='Keep the already aquired data, defaults to False')
-def acquire_data(output_directory, log, loglevel, keep):
-    if log is not None:
-        logging.basicConfig(filename=log, level=_log_level_dict[loglevel],
-                            format='[%(asctime)s] %(levelname)s:'
-                                   '%(name)-50s %(message)s')
-    logger = logging.getLogger('main')
-    # get the expected files from the directory
-    output_directory = Path(output_directory)
-    network_config = output_directory / 'network_config.yaml'
-    if not network_config.exists():
-        print(f"No network_config.yaml found in {output_directory}"
-              "exiting ...")
-        exit(1)
-    default_config = output_directory / 'default_config.yaml'
-    if not default_config.exists():
-        print(f"No default_config.yaml found in {output_directory}"
-              "exiting ...")
-        exit(1)
-    init_config = output_directory / 'initial_state_config.yaml'
-    if not init_config.exists():
-        print(f"No initial_state_config.yaml found in {output_directory}"
-              "exiting ...")
-        exit(1)
-    run_config_files = list(glob.glob(
-        str(output_directory.absolute()) + '/' + 'run_*_config.yaml'))
-    sorted(run_config_files,
-           key=lambda x: int(os.path.basename(x).split('_')[1]))
-    run_indices = list(map(lambda x: os.path.basename(x).split('_')[1],
-                           run_config_files))
-
-    # read in the configurations
-    logger.info('Reading in configurations')
-    with open(network_config, 'r') as nwcf:
-        network_config = yaml.safe_load(nwcf.read())
-    with open(default_config, 'r') as dcf:
-        default_config = yaml.safe_load(dcf.read())
-    with open(init_config, 'r') as icf:
-        init_config = yaml.safe_load(icf.read())
-    run_configs = []
-    with Progress(
-            SpinnerColumn(),
-            TextColumn("[progress.description]{task.description}"),
-            BarColumn(bar_width=None),
-            MofNCompleteColumn(),
-            TimeElapsedColumn(),
-            TimeRemainingColumn()) as progress:
-        read_configurations = progress.add_task(
-                "[cyan] Read run configurations from disk",
-                total=len(run_indices))
-        for rcfp in run_config_files:
-            with open(rcfp, 'r') as rcf:
-                run_configs.append(yaml.safe_load(rcf.read()))
-            progress.update(read_configurations, advance=1)
-        # instantiate client and server
-        # initialize the daq system
-        acquire_data_progbar = progress.add_task(
-                "[blue]Acquiring Data from the Hexacontroller",
-                total=len(run_indices),
-                start=False
-                )
-        daq_system = DAQCoordClient(network_config)
-        daq_system.initialize(init_config)
-        # setup data taking context for the client
-
-        # delete the old raw files
-        if not keep:
-            old_raw_files = glob.glob(
-                    str(output_directory.absolute())+'/*.raw')
-            if len(old_raw_files):
-                print('Deleting old Data')
-            for file in old_raw_files:
-                os.remove(file)
-
-        # take the data
-        progress.start_task(acquire_data_progbar)
-        for index, run in zip(run_indices, run_configs):
-            output_file = output_directory / f'run_{index}_data.raw'
-            if not keep or (keep and not output_file.exists()):
-                data = daq_system.measure(run)
-                with open(output_file, 'wb+') as df:
-                    df.write(data)
-            progress.update(acquire_data_progbar, advance=1)
-
-
 def pipelined_acquire_data(configurations: queue.Queue,
                            data_acquisition_progress: queue.Queue,
                            acquired_data: queue.Queue,
@@ -129,13 +33,15 @@ def pipelined_acquire_data(configurations: queue.Queue,
                            output_dir: Path,
                            run_count: int,
                            keep: bool,
-                           readback: bool):
+                           readback: bool,
+                           start_tdcs: bool,
+                           full_readback: bool):
     # info needed for the generation of the filenames
     num_digits = math.ceil(math.log(run_count, 10))
     logger = logging.getLogger('data-acquisitor')
     logger.info('Initializing DAQ system')
     daq_system = DAQCoordClient(network_configuration)
-    daq_system.initialize(initial_config)
+    daq_system.initialize(initial_config, start_tdcs=start_tdcs)
     daq_initialized.set()
     logger.info('DAQ-System initialized')
     i = 0
@@ -149,6 +55,9 @@ def pipelined_acquire_data(configurations: queue.Queue,
             'run_{0:0>{width}}_data.root'.format(i, width=num_digits)
         hdf_file_name = \
             'run_{0:0>{width}}_data.h5'.format(i, width=num_digits)
+        full_roc_readback_file_name = \
+            'run_{0:0>{width}}_config_readback.yaml'.format(i,
+                                                            width=num_digits)
         if not keep or (keep and not raw_file_path.exists()):
             logger.info(f'gathering Data for run {i}')
             try:
@@ -161,6 +70,16 @@ def pipelined_acquire_data(configurations: queue.Queue,
                 return
             with open(output_dir / raw_file_name, 'wb+') as rdf:
                 rdf.write(data)
+            try:
+                config = daq_system.read_target(config=None)
+            except ValueError as err:
+                click.echo('An error occured during readout of the target:'
+                           f' {err.args[0]}')
+                del daq_system
+                data_acquisition_done.set()
+                return
+            with open(output_dir / full_roc_readback_file_name, 'w+') as frcf:
+                frcf.write(yaml.safe_dump(config))
         else:
             logger.info(f'found existing data for run {i}, '
                         'skipping acquisition')
@@ -198,14 +117,21 @@ def pipelined_acquire_data(configurations: queue.Queue,
 @click.option('--readback/--no-readback', default=False,
               help='Tell the sc-server to read back the register value'
                    'written to the roc')
-def pipelined_main(output_directory, log, loglevel, keep, readback):
-    if log is not None:
-        logging.basicConfig(filename=log, level=_log_level_dict[loglevel],
-                            format='[%(asctime)s] %(levelname)s:'
-                                   '%(name)-50s %(message)s')
+@click.option('--full-readback/-no-full-readback', default=False,
+              help='Enable a Full read of the ROC parameters after every run')
+def pipelined_main(output_directory, log, loglevel, keep, readback,
+                   full_readback):
+    logging.basicConfig(filename=log, level=_log_level_dict[loglevel],
+                        format='[%(asctime)s] %(levelname)s:'
+                               '%(name)-50s %(message)s')
     logger = logging.getLogger('main')
     # get the expected files from the directory
     output_directory = Path(output_directory)
+    daq_config = output_directory / 'daq_config.yaml'
+    if not daq_config.exists():
+        print(f"No daq_config.yaml found in {output_directory}"
+              "exiting ...")
+        exit(1)
     network_config = output_directory / 'network_config.yaml'
     if not network_config.exists():
         print(f"No network_config.yaml found in {output_directory}"
@@ -240,6 +166,8 @@ def pipelined_main(output_directory, log, loglevel, keep, readback):
         default_config = yaml.safe_load(dcf.read())
     with open(init_config, 'r') as icf:
         init_config = yaml.safe_load(icf.read())
+    with open(daq_config, 'r') as daqcf:
+        daq_config = yaml.safe_load(daqcf.read())
     # set up the different events that we are looking for
     all_run_configs_generated = threading.Event()
     daq_system_initialized = threading.Event()
@@ -265,6 +193,8 @@ def pipelined_main(output_directory, log, loglevel, keep, readback):
                   len(run_indices),
                   keep,
                   readback,
+                  daq_config['run_start_tdc_procedure'],
+                  full_readback,
                   )
             )
     daq_thread.start()
