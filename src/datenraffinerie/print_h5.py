91aa2fa6c3dc36198baf5d5f0b78cde909700ffb
Alexande Becker <a.becker@cern.ch>
2022-09-13 16:06:51 +0200

    added some options for printing the hdf-file

--- src/datenraffinerie/print_h5.py
+++ src/datenraffinerie/print_h5.py
@@ -7,12 +7,14 @@ from rich import print
 @click.command()
 @click.argument('hdf-file', type=click.Path(exists=True),
                 metavar='[HDF-DATA]')
-@click.option('-+rows', '-r', type=int, default=None)
-def show_hdf(hdf_file, rows):
+@click.option('--rows', '-r', type=int, default=50)
+@click.option('--start', '-s', type=int, default=0)
+def show_hdf(hdf_file, rows, start):
+    pd.set_option('display.max_rows', None)
+    pd.set_option('display.max_columns', None)
+    pd.set_option('display.width', None)
+    pd.set_option('display.max_colwidth', None)
     tb = tables.open_file(hdf_file)
     data = tb.root.data.measurements
     df = pd.DataFrame.from_records(data.read())
-    if rows is not None:
-        print(df.head(rows))
-    else: 
-        print(df)
+    print(df.iloc[start:start+rows])
