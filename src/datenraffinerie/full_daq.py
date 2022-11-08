7ee78fb611d47483a9cd7a08f7047918e2b8ec94
Alexande Becker <a.becker@cern.ch>
2022-11-08 20:11:53 +0100

    reformatting

--- src/datenraffinerie/full_daq.py
+++ src/datenraffinerie/full_daq.py
@@ -25,64 +25,125 @@ from copy import deepcopy
 from operator import and_
 
 
-_log_level_dict = {'DEBUG': logging.DEBUG,
-                   'INFO': logging.INFO,
-                   'WARNING': logging.WARNING,
-                   'ERROR': logging.ERROR,
-                   'CRITICAL': logging.CRITICAL}
+_log_level_dict = {
+    "DEBUG": logging.DEBUG,
+    "INFO": logging.INFO,
+    "WARNING": logging.WARNING,
+    "ERROR": logging.ERROR,
+    "CRITICAL": logging.CRITICAL,
+}
 
 
 @click.command()
-@click.argument('config', type=click.Path(exists=True),
-                metavar='[ROOT CONFIG FILE]')
-@click.argument('netcfg', type=click.Path(exists=True),
-                metavar='[NERTWORK CONFIG FILE]')
-@click.argument('procedure', type=str,
-                metavar='[PROCEDURE NAME]')
-@click.argument('output_dir', type=click.Path(dir_okay=True),
-                metavar='[OUTPUT DIRECTORY]')
-@click.option('--log', '-l', type=str, default='daq.log',
-              help='Enable logging and append logs to the filename passed to '
-                   'this option')
-@click.option('--loglevel', '-v', default='INFO',
-              type=click.Choice(['DEBUG', 'INFO',
-                                 'WARNING', 'ERROR', 'CRITICAL'],
-                                case_sensitive=False))
-@click.option('--root/--no-root', default=False,
-              help='keep the rootfile generated as intermediary')
-@click.option('--full_conf_generators', '-f', type=int, default=2,
-              help='set how many full-cfg generators to run, default 1')
-@click.option('--unpack_tasks', '-u', default=2, type=int,
-              help='number of unpackers/frackers to run in parallel')
-@click.option('--frack_tasks', '-f', default=7, type=int,
-              help='number of unpackers/frackers to run in parallel')
-@click.option('--compression', '-c', default=2, type=int,
-              help='Set the compression for the hdf file, 0 = no compression'
-                   ' 9 = best compression')
-@click.option('--keep/--no-keep', default=False,
-              help='Keep the already aquired data, defaults to False')
-@click.option('--readback/--no-readback', default=False,
-              help='Tell the sc-server to read back the register value'
-                   'written to the roc')
-@click.option('--full-readback/-no-full-readback', default=False,
-              help='Enable a Full read of the ROC parameters after every run')
-def main(config, netcfg, procedure, output_dir, log, loglevel,
-         root, full_conf_generators, unpack_tasks, fracker_tasks,
-         compression, keep, readback, full_readback):
+@click.argument("config", type=click.Path(exists=True), metavar="[ROOT CONFIG FILE]")
+@click.argument(
+    "netcfg", type=click.Path(exists=True), metavar="[NERTWORK CONFIG FILE]"
+)
+@click.argument("procedure", type=str, metavar="[PROCEDURE NAME]")
+@click.argument(
+    "output_dir", type=click.Path(dir_okay=True), metavar="[OUTPUT DIRECTORY]"
+)
+@click.option(
+    "--log",
+    "-l",
+    type=str,
+    default="daq.log",
+    help="Enable logging and append logs to the filename passed to " "this option",
+)
+@click.option(
+    "--loglevel",
+    "-v",
+    default="INFO",
+    type=click.Choice(
+        ["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"], case_sensitive=False
+    ),
+)
+@click.option(
+    "--root/--no-root",
+    default=False,
+    help="keep the rootfile generated as intermediary",
+)
+@click.option(
+    "--full_conf_generators",
+    "-f",
+    type=int,
+    default=2,
+    help="set how many full-cfg generators to run, default 1",
+)
+@click.option(
+    "--unpack_tasks",
+    "-u",
+    default=2,
+    type=int,
+    help="number of unpackers/frackers to run in parallel",
+)
+@click.option(
+    "--frack_tasks",
+    "-f",
+    default=7,
+    type=int,
+    help="number of unpackers/frackers to run in parallel",
+)
+@click.option(
+    "--compression",
+    "-c",
+    default=2,
+    type=int,
+    help="Set the compression for the hdf file, 0 = no compression"
+    " 9 = best compression",
+)
+@click.option(
+    "--keep/--no-keep",
+    default=False,
+    help="Keep the already aquired data, defaults to False",
+)
+@click.option(
+    "--readback/--no-readback",
+    default=False,
+    help="Tell the sc-server to read back the register value" "written to the roc",
+)
+@click.option(
+    "--full-readback/-no-full-readback",
+    default=False,
+    help="Enable a Full read of the ROC parameters after every run",
+)
+def main(
+    config,
+    netcfg,
+    procedure,
+    output_dir,
+    log,
+    loglevel,
+    root,
+    full_conf_generators,
+    unpack_tasks,
+    fracker_tasks,
+    compression,
+    keep,
+    readback,
+    full_readback,
+):
     config = click.format_filename(config)
     # generate the conifgurations
     if log:
-        logging.basicConfig(filename='full-daq.log', level=loglevel,
-                            format='[%(asctime)s] %(levelname)-10s:'
-                                   '%(name)-50s %(message)s')
+        logging.basicConfig(
+            filename="full-daq.log",
+            level=loglevel,
+            format="[%(asctime)s] %(levelname)-10s:" "%(name)-50s %(message)s",
+        )
     config = click.format_filename(config)
     try:
-        procedure, (system_default_config, system_init_config, run_configs,
-                    run_count) = cfu.get_procedure_configs(
-                            main_config_file=config,
-                            procedure_name=procedure,
-                            calibration=None,
-                            diff=True)
+        procedure, (
+            system_default_config,
+            system_init_config,
+            run_configs,
+            run_count,
+        ) = cfu.get_procedure_configs(
+            main_config_file=config,
+            procedure_name=procedure,
+            calibration=None,
+            diff=True,
+        )
         run_configs_1, run_configs_2 = tee(run_configs)
     except ValueError as err:
         print(f"The procedure with name: {err.args[1]} could not be found,")
@@ -96,18 +157,18 @@ def main(config, netcfg, procedure, output_dir, log, loglevel,
     if not os.path.exists(output_dir):
         os.mkdir(output_dir)
 
-    generate_tool_configurations(output_dir, netcfg, procedure,
-                                 system_default_config, system_init_config)
+    generate_tool_configurations(
+        output_dir, netcfg, procedure, system_default_config, system_init_config
+    )
 
     # get the data needed for the postprocessing
-    raw_data = True if procedure['mode'] == 'full' else False
-    data_columns = procedure['data_columns']
+    raw_data = True if procedure["mode"] == "full" else False
+    data_columns = procedure["data_columns"]
     try:
-        data_format = procedure['data_format']
+        data_format = procedure["data_format"]
     except KeyError:
-        data_format = 'raw'
-    characterisation_mode = True \
-        if data_format == 'characterisation' else False
+        data_format = "raw"
+    characterisation_mode = True if data_format == "characterisation" else False
 
     # generate the configurations for the runs
     num_digits = math.ceil(math.log(run_count, 10))
@@ -124,8 +185,9 @@ def main(config, netcfg, procedure, output_dir, log, loglevel,
     run_config_queue = queue.Queue()
     # queues containing the run configs for the different full config
     # generators
-    full_config_generator_input_queues = \
-        [mp.Queue() for _ in range(full_conf_generators)]
+    full_config_generator_input_queues = [
+        mp.Queue() for _ in range(full_conf_generators)
+    ]
     # queue holding the tasks representing the task after
     # data taking
     raw_data_queue = queue.Queue()
@@ -177,98 +239,107 @@ def main(config, netcfg, procedure, output_dir, log, loglevel,
 
     # events signalling that the different config generators have finished
     # generating configurations
-    full_config_generators_done = [
-            mp.Event() for _ in range(full_conf_generators)]
+    full_config_generators_done = [mp.Event() for _ in range(full_conf_generators)]
 
     config_gen_thread = threading.Thread(
-            target=pipelined_generate_run_params,
-            args=(output_dir,                       # directory to place the
-                                                    # results into
-                  run_configs_2,                    # the following two items
-                                                    # are used by the
-                  config_iter_lock,                 # config generator to build
-                                                    # the differential run
-                                                    # configurations
-                  run_config_queue,
-                  run_config_gen_progress_queue,
-                  run_config_gen_done,
-                  num_digits
-                  )
-            )
-    full_config_gen_procs = [mp.Process(
+        target=pipelined_generate_run_params,
+        args=(
+            output_dir,  # directory to place the
+            # results into
+            run_configs_2,  # the following two items
+            # are used by the
+            config_iter_lock,  # config generator to build
+            # the differential run
+            # configurations
+            run_config_queue,
+            run_config_gen_progress_queue,
+            run_config_gen_done,
+            num_digits,
+        ),
+    )
+    full_config_gen_procs = [
+        mp.Process(
             target=generate_full_configs,
-            args=(output_dir,
-                  rcq,
-                  config_queue_fill_done,
-                  full_config,
-                  num_digits,
-                  full_configs_queue,
-                  full_config_generation_progress,
-                  fcgd,
-                  stop
-                  )
-            ) for rcq, fcgd in zip(full_config_generator_input_queues,
-                                   full_config_generators_done)]
+            args=(
+                output_dir,
+                rcq,
+                config_queue_fill_done,
+                full_config,
+                num_digits,
+                full_configs_queue,
+                full_config_generation_progress,
+                fcgd,
+                stop,
+            ),
+        )
+        for rcq, fcgd in zip(
+            full_config_generator_input_queues, full_config_generators_done
+        )
+    ]
     # acquire the data in a separate thread
     daq_thread = threading.Thread(
-            target=pipelined_acquire_data,
-            args=(run_config_queue,
-                  daq_progress,
-                  raw_data_queue,
-                  run_config_gen_done,
-                  daq_system_initialized,
-                  data_acquisition_done,
-                  stop,
-                  netcfg,
-                  system_init_config,
-                  output_dir,
-                  run_count,
-                  keep,
-                  readback,
-                  procedure['run_start_tdc_procedure'],
-                  full_readback,
-                  )
-            )
+        target=pipelined_acquire_data,
+        args=(
+            run_config_queue,
+            daq_progress,
+            raw_data_queue,
+            run_config_gen_done,
+            daq_system_initialized,
+            data_acquisition_done,
+            stop,
+            netcfg,
+            system_init_config,
+            output_dir,
+            run_count,
+            keep,
+            readback,
+            procedure["run_start_tdc_procedure"],
+            full_readback,
+        ),
+    )
 
     # start the unpack_tasks that turn the raw data into the root file
     unpack_thread = threading.Thread(
-            target=unpack_data,
-            args=(raw_data_queue,
-                  unpack_progress,
-                  root_data_queue,
-                  data_acquisition_done,
-                  unpack_done,
-                  max(1, unpack_tasks),
-                  raw_data,
-                  characterisation_mode
-                  )
-            )
+        target=unpack_data,
+        args=(
+            raw_data_queue,
+            unpack_progress,
+            root_data_queue,
+            data_acquisition_done,
+            unpack_done,
+            max(1, unpack_tasks),
+            raw_data,
+            characterisation_mode,
+        ),
+    )
 
     # synchronize the fracking with the full config generation
     synchronize_with_full_config_generation = threading.Thread(
-            target=synchronize_with_config_generation,
-            args=(root_data_queue,
-                  syncronized_root_data_queue,
-                  full_configs_queue,
-                  unpack_done,
-                  sync_done
-                  )
-            )
+        target=synchronize_with_config_generation,
+        args=(
+            root_data_queue,
+            syncronized_root_data_queue,
+            full_configs_queue,
+            unpack_done,
+            sync_done,
+        ),
+    )
 
     # frack the data (last step)
     frack_thread = threading.Thread(
-            target=frack_data,
-            args=(syncronized_root_data_queue,
-                  frack_progress,
-                  unpack_done,
-                  fracking_done,
-                  max(1, fracker_tasks),
-                  raw_data,
-                  root,
-                  compression,
-                  data_columns,
-                  )
-            )
+        target=frack_data,
+        args=(
+            syncronized_root_data_queue,
+            frack_progress,
+            unpack_done,
+            fracking_done,
+            max(1, fracker_tasks),
+            raw_data,
+            root,
+            compression,
+            data_columns,
+        ),
+    )
     config_gen_thread.start()
     for fcgp in full_config_gen_procs:
         fcgp.start()
@@ -277,47 +348,42 @@ def main(config, netcfg, procedure, output_dir, log, loglevel,
     synchronize_with_full_config_generation.start()
     frack_thread.start()
     with Progress(
-            SpinnerColumn(),
-            TextColumn("[progress.description]{task.description}"),
-            BarColumn(bar_width=None),
-            MofNCompleteColumn(),
-            TimeElapsedColumn(),
-            TimeRemainingColumn()) as progress:
+        SpinnerColumn(),
+        TextColumn("[progress.description]{task.description}"),
+        BarColumn(bar_width=None),
+        MofNCompleteColumn(),
+        TimeElapsedColumn(),
+        TimeRemainingColumn(),
+    ) as progress:
         run_config_progress_bar = progress.add_task(
-                'Generating run configurations',
-                total=run_count)
+            "Generating run configurations", total=run_count
+        )
         full_config_progress_bar = progress.add_task(
-                'Generating full config for fracker',
-                total=run_count)
+            "Generating full config for fracker", total=run_count
+        )
         daq_progbar = progress.add_task(
-                "Acquiring Data from the Test System",
-                total=run_count,
-                start=False
-                )
-        unpack_progress_bar = progress.add_task(
-            'Unpacking data',
-            total=run_count)
-        frack_progress_bar = progress.add_task(
-            'fracking data',
-            total=run_count)
+            "Acquiring Data from the Test System", total=run_count, start=False
+        )
+        unpack_progress_bar = progress.add_task("Unpacking data", total=run_count)
+        frack_progress_bar = progress.add_task("fracking data", total=run_count)
         i = 0
         run_config = {}
         # current configuration state of the config generator
         generator_config_states = [{} for _ in range(full_conf_generators)]
-        while not config_queue_fill_done.is_set() \
-                or not run_config_gen_progress_queue.empty() \
-                or not run_config_queue.empty() \
-                or not run_config_gen_done.is_set()\
-                or not reduce(and_, map(lambda x: x.is_set(),
-                                        full_config_generators_done)):
+        while (
+            not config_queue_fill_done.is_set()
+            or not run_config_gen_progress_queue.empty()
+            or not run_config_queue.empty()
+            or not run_config_gen_done.is_set()
+            or not reduce(and_, map(lambda x: x.is_set(), full_config_generators_done))
+        ):
             try:
                 for j, (rcq, gstate) in enumerate(
-                        zip(full_config_generator_input_queues,
-                            generator_config_states)):
+                    zip(full_config_generator_input_queues, generator_config_states)
+                ):
                     with config_iter_lock:
                         rcf_update = deepcopy(next(run_configs_1))
-                    dctu.update_dict(run_config,
-                                     rcf_update, in_place=True)
+                    dctu.update_dict(run_config, rcf_update, in_place=True)
                     new_state = dctu.update_dict(gstate, run_config)
                     rcq.put((i, new_state))
                     i += 1
@@ -341,8 +407,9 @@ def main(config, netcfg, procedure, output_dir, log, loglevel,
                 except queue.Empty:
                     break
             logging.debug(
-                f'{reduce(and_, map(lambda x: x.is_set(), full_config_generators_done))}'
-                ' = state of full config generators')
+                f"{reduce(and_, map(lambda x: x.is_set(), full_config_generators_done))}"
+                " = state of full config generators"
+            )
 
         progress.refresh()
     config_gen_thread.join()
